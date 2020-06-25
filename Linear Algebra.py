ff639d2e386fb1cc47abde487a7b1a5361c926d2
luiscasado620@gmail.com <Luiscasado620@gmail.com>
2020-06-24 20:32:42 -0400

    Linear Algebra with Python

---
+++ Linear Algebra.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+3+2
+
+
+# In[3]:
+
+
+a = np.arange(15).reshape(3, 5)
+a
+
+
+# In[4]:
+
+
+a.shape
+
+
+# In[5]:
+
+
+a.dtype
+
+
+# In[6]:
+
+
+a.size
+
+
+# In[7]:
+
+
+type(a)
+
+
+# In[8]:
+
+
+b = np.array([6, 7, 8])
+b
+
+
+# In[9]:
+
+
+b.dtype
+
+
+# In[11]:
+
+
+np.ones((3,4))
+
+
+# In[ ]:
+
+
+
+
